b8dec0bafe15191c73271c9709e403a5f5b0aa28
Wilshawn Alexander <wilshawn.alexander@gmail.com>
2023-12-06 14:35:30 -0500

    Production

---
+++ src/Framework/Rules/UrlRule.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework\Rules;
+
+use Framework\Contracts\RuleInterface;
+
+class UrlRule implements RuleInterface
+{
+    public function validate(array $data, string $field, array $param): bool
+    {
+        return (bool) filter_var($data[$field], FILTER_VALIDATE_URL);
+    }
+
+    public function getMessage(array $data, string $field, array $param): string
+    {
+        return "Invalid URL";
+    }
+}
